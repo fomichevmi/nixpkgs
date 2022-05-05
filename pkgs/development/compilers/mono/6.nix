a04ede2b24c290a73b40edc0464175e3947d4f34
fomichevmi <59839128+fomichevmi@users.noreply.github.com>
2022-05-05 17:04:53 +0200

    mono6: 6.12.0.122 -> 6.12.0.171

--- pkgs/development/compilers/mono/6.nix
+++ pkgs/development/compilers/mono/6.nix
@@ -2,8 +2,8 @@
 
 callPackage ./generic.nix ({
   inherit Foundation libobjc;
-  version = "6.12.0.122";
+  version = "6.12.0.171";
   srcArchiveSuffix = "tar.xz";
-  sha256 = "sha256-KcJ3Zg/F51ExB67hy/jFBXyTcKTN/tovx4G+aYbYnSM=";
+  sha256 = "0a025x5b3i8mswz9lb7hbmwhc1lis1ylwjpx3iqmbzd26s9fbgdc";
   enableParallelBuilding = true;
 })
